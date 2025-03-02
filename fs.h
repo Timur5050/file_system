6937a2973108ff166c19ec08bc192e9a6ad2acc5
Timur <stukantimur811@gmail.com>
2025-03-02 21:13:05 +0200

    headed for file system

---
+++ fs.h
@@ -0,0 +1,57 @@
+#ifndef FS_H
+#define FS_H
+
+#include <stdint.h>
+
+#define BLOCK_SIZE 4096  
+#define INODE_BLOCKS 5
+#define DATA_BLOCKS 56
+#define BITMAP_INODE_BLOCKS 1
+#define BITMAP_DATA_BLOCKS 1
+#define SUPERBLOCKS 1
+
+#define TOTAL_BLOCKS (INODE_BLOCKS + DATA_BLOCKS + BITMAP_INODE_BLOCKS + BITMAP_DATA_BLOCKS + SUPERBLOCKS)
+
+typedef struct superblock {
+    uint32_t total_blocks;
+    uint32_t inode_start;
+    uint32_t data_start;
+    uint32_t inode_bitmap_start;
+    uint32_t data_bitmap_start;
+    uint32_t inode_count;
+} superblock;
+
+typedef struct inode {
+    uint16_t mode;
+    uint16_t uid;
+    uint32_t size;
+    uint32_t mtime;
+    uint16_t gid;
+    uint16_t links_count;
+    uint32_t blocks;
+    uint32_t block[8]; 
+    uint32_t indirect_block;
+} inode;
+
+typedef struct dir_entry {
+    uint32_t inum;
+    uint16_t reclen;
+    uint8_t strlen;
+    char name[64]; 
+} dir_entry;
+
+typedef struct block_entry {
+    uint32_t data[1024];
+} block;
+
+struct disk_mem {
+    superblock disk_superblock;
+    int i_bmap[1024];
+    int d_bmap[1024];
+    inode inode_list[5];
+    void* block_list[56];
+};
+
+void init_superblock(superblock *sb);
+
+#endif // FS_H
