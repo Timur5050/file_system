17600698958215aa1bc9a4950414f91d745123b2
Timur <stukantimur811@gmail.com>
2025-03-04 23:09:36 +0200

    test whether initialization works correctly

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include "fs.h"
 
+
+typedef struct dir1 {
+    int *a;
+} dir1;
+
 int main() {
     superblock sb;
     init_superblock(&sb);
 
-    printf("Total blocks: %u\n", sb.total_blocks);
-    printf("Inode start: %u\n", sb.inode_start);
-    printf("Data start: %u\n", sb.data_start);
-
+    //printf("Total blocks: %u\n", sb.total_blocks);
+    //printf("Inode start: %u\n", sb.inode_start);
+    //printf("Data start: %u\n", sb.data_start);
+    
+    disk_mem* dm = (disk_mem*)malloc(sizeof(disk_mem));
+    init_disk_mem(dm);
+    init_root_directory(dm);
+    
     return 0;
 }
