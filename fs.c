5150f00dfb27fb0002af39496310896c8d512d44
Timur <stukantimur811@gmail.com>
2025-03-04 23:09:08 +0200

    added hard drive structure initialization and root dir initialization

--- fs.c
+++ fs.c
@@ -8,3 +8,72 @@ void init_superblock(superblock *sb) {
     sb->data_bitmap_start = SUPERBLOCKS + BITMAP_INODE_BLOCKS;
     sb->inode_count = INODE_BLOCKS * (BLOCK_SIZE / sizeof(inode));
 }
+
+void init_root_directory(disk_mem *dm)
+{
+    dir* root_dir = (dir*)malloc(sizeof(dir));
+    for(int i = 0; i < (BLOCK_SIZE / sizeof(dir_entry)); i++)
+    {
+        root_dir->data[i].inum = -1;
+    }
+
+    int data_block_ind = 0;
+    dm->block_list[data_block_ind] = (dir*)root_dir;
+    
+
+    inode* root_inode = (inode*)malloc(sizeof(inode));
+    root_inode->uid = 0;
+    root_inode->mode = 1;
+    root_inode->size = sizeof(dir);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    root_inode->mtime = tv.tv_sec;
+    root_inode->links_count = 0;
+    root_inode->direct_blocks = (uint32_t*)malloc(sizeof(uint32_t) * DIRECT_BLOCKS);
+    root_inode->direct_blocks[0] = 0;
+    for(int i = 1; i < DIRECT_BLOCKS; i++)
+    {
+        root_inode->direct_blocks[i] = -1;
+    } 
+    root_inode->indirect_block = (uint32_t*)malloc(sizeof(uint32_t) * INDIRECT_BLOCKS);
+    for(int i = 0; i < INDIRECT_BLOCKS; i++)
+    {
+        root_inode->indirect_block[i] = -1;
+    }
+
+    root_dir->data[0].inum = 0;
+    root_dir->data[0].reclen = sizeof(dir_entry);
+    root_dir->data[0].strlen = 1;
+    strcpy(root_dir->data[0].name, ".");
+}
+
+
+void init_disk_mem(disk_mem* dm)
+{
+    superblock* sb = (superblock*)malloc(sizeof(superblock));
+    init_superblock(sb);
+    
+    uint8_t* i_mp = (uint8_t*)calloc(BITMAP_INODE_BLOCKS * BLOCK_SIZE, sizeof(uint8_t));
+    uint8_t* d_mp = (uint8_t*)calloc(BITMAP_DATA_BLOCKS * BLOCK_SIZE, sizeof(uint8_t));
+
+    
+    inode** inode_ls = (inode**)malloc(sizeof(inode*) * INODE_BLOCKS);
+    for(int i = 0; i < INODE_BLOCKS; i++)
+    {
+        inode_ls[i] = (inode*)malloc(sizeof(inode));
+    }
+    void** block_ls = (void**)malloc(sizeof(void*) * DATA_BLOCKS);
+    for(int i = 0; i < DATA_BLOCKS; i++)
+    {
+        block_ls[i] = NULL;
+    }
+    for(int i = 0; i < INODE_BLOCKS; i++) {
+        inode_ls[i] = NULL; 
+    }
+
+    dm->disk_superblock = sb;
+    dm->i_bmap = i_mp;
+    dm->d_bmap = d_mp;
+    dm->inode_list = inode_ls;
+    dm->block_list = block_ls;
+}
