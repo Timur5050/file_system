6b20def40654606a0357ac7f5d18f59e6f48d455
Timur <stukantimur811@gmail.com>
2025-03-07 23:57:05 +0200

    added new func to review entries from directory and to review all blocks from dm

--- fs.c
+++ fs.c
@@ -158,15 +158,112 @@ void print_all_inodes(disk_mem* dm)
 
 void print_all_blocks(disk_mem* dm)
 {
-    for(int i = 0; i < dm->block_list; i++)
-    {
+    printf("---- block list ----\n");
 
+    for(int block_index = 0; block_index < DATA_BLOCKS; block_index++)
+    {
+        void* block = dm->block_list[block_index];
+        if(NULL == block)
+        {
+            continue;
+        }
+
+        inode* owner_inode = NULL;
+        for(int j = 0; j < INDIRECT_BLOCKS; j++)
+        {
+            inode* temp_inode = dm->inode_list[j];
+
+            if(NULL == temp_inode)
+            {
+                continue;
+            }
+
+            for(int k = 0; k < DIRECT_BLOCKS; k++)
+            {
+                if(temp_inode->direct_blocks[k] == block_index)
+                {
+                    owner_inode = temp_inode;
+                    break;
+                }
+            }
+
+            for(int k = 0; k < INDIRECT_BLOCKS; k++)
+            {
+                if(temp_inode->indirect_block[k] == block_index)
+                {
+                    owner_inode = temp_inode;
+                    break;
+                }
+            }
+
+            if(owner_inode)
+            {
+                break;
+            }
+
+        }
+
+        if(owner_inode)
+        {
+            if(owner_inode->mode == 1) // dir
+            {
+                dir* directory = (dir*)block;
+                printf("block %d: dir\n", block_index);
+
+                for(int j = 0; j < (BLOCK_SIZE / sizeof(dir_entry)); j++)
+                {
+                    if(directory->data[j].inum != -1)
+                    {
+                        printf(" %d %s (inode: %d)\n", j, directory->data[j].name, directory->data[j].inum);
+                    }
+                }
+            }
+            else //file
+            {
+                printf("block %d: file data\n", block_index);
+            }
+        }
+        else
+        {
+            printf("block %d: unknown type\n", block_index);
+        }
     }
 }
 
-// void print_all_dir_entries(disk_mem* dm, uint32_t *inode_number_of_dir)
-// {
+void print_all_dir_entries(disk_mem* dm, uint32_t inode_number_of_dir)
+{
+    inode *dir_inode = dm->inode_list[inode_number_of_dir];
     
-// }
+    if(dir_inode->mode != 1)
+    {
+        printf("not a dir! \n");
+    }
+    else
+    {
+        for(int i = 0; i < DIRECT_BLOCKS; i++)
+        {
+            uint32_t curr_dir_inum = dir_inode->direct_blocks[i];
+            dir* curr_dir = (dir*)dm->block_list[curr_dir_inum];
+
+            for(int j = 0; j < BLOCK_SIZE / sizeof(dir_entry); j++)
+            {
+                dir_entry curr_dir_entry = curr_dir->data[j];
+
+                if(curr_dir_entry.inum == -1)
+                {
+                    continue;
+                }
+
+                printf("inum : %d, reclen : %d, strlen : %d, name : %s\n", 
+                curr_dir_entry.inum,
+                curr_dir_entry.reclen,
+                curr_dir_entry.strlen,
+                curr_dir_entry.name
+                );
+            }
+        }
+    }
+
+}
 
 
