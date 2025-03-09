52f3d3f43356160214cef4bed1e0d30fa478acbd
Timur <stukantimur811@gmail.com>
2025-03-09 19:44:41 +0200

    added tests for testing wheter previously created funcs been done well, esspecially creation files and adding that files to dir

--- main.c
+++ main.c
@@ -24,6 +24,26 @@ int main() {
 
     print_all_dir_entries(dm, 0);
 
+    char file_name[30] = "hello.txt";
+    uint32_t *data = (uint32_t*)malloc(sizeof(uint32_t) * 10);
+    for(uint32_t i = 0; i < 10; i++)
+    {   
+        *(data + i) = i;
+    }  
+
+    int res = create_file(
+        dm,
+        0,
+        file_name,
+        data
+    );
+    printf("\n\n added file test : \n\n");
+    print_all_inodes(dm);
+    printf("\n\n");
+    print_all_blocks(dm);
+    printf("\n\n");
+
+    print_all_dir_entries(dm, 0);
 
     return 0;
 }
