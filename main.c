de9b99893c183720141c4521e64dafff86cdb890
Timur <stukantimur811@gmail.com>
2025-03-10 23:29:36 +0200

    changed tests a little bit

--- main.c
+++ main.c
@@ -17,10 +17,7 @@ int main() {
     disk_mem* dm = (disk_mem*)malloc(sizeof(disk_mem));
     init_disk_mem(dm);
     init_root_directory(dm);
-    print_all_inodes(dm);
-    printf("\n\n");
-    print_all_blocks(dm);
-    printf("\n\n");
+    print_whole_dm(dm);
 
     print_all_dir_entries(dm, 0);
 
@@ -38,11 +35,7 @@ int main() {
         data
     );
     printf("\n\n added file test : \n\n");
-    print_all_inodes(dm);
-    printf("\n\n");
-    print_all_blocks(dm);
-    printf("\n\n");
-
+    print_whole_dm(dm);
     print_all_dir_entries(dm, 0);
 
     return 0;
