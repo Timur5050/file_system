bdc5e81222dab225318e362c303b7874eb73cb08
Timur <stukantimur811@gmail.com>
2025-03-17 22:21:00 +0200

    tests

--- main.c
+++ main.c
@@ -22,11 +22,10 @@ int main() {
     print_all_dir_entries(dm, 0);
 
     char file_name[30] = "hello.txt";
-    uint32_t *data = (uint32_t*)malloc(sizeof(uint32_t) * 2000);
-    for(uint32_t i = 0; i < 2000; i++)
-    {   
-        *(data + i) = i;
-    }  
+    char *data = (char*)malloc(5000);
+    for (int i = 0; i < 5000; i++) {
+        data[i] = 'A' + (i % 26);
+    }
     // int res = create_file(
     //     dm,
     //     0,
@@ -60,7 +59,9 @@ int main() {
         1,
         file_name
     );
-    add_data_to_file(dm, 2, data, 2000);
+    add_data_to_file(dm, 2, data, 5000);
+    // print_whole_data_from_file(dm, 2);
+    // printf("\n");
     // printf("\n\n added dir test : \n\n");
     // print_whole_dm(dm);
 
@@ -74,10 +75,31 @@ int main() {
     // printf("\n\n added dir test : \n\n");
     // print_whole_dm(dm);
 
-    char text[1000] = "";
-    pwd(dm, 1, text);
+    // print_whole_dm(dm);
 
-    printf("\n%s\n\n", text);
-    ls(dm, 1);
+    int curr_inode = 1;
+    char text[1000] = "";
+    pwd(dm, curr_inode, text);
+
+    printf("\n%s\n", text);
+    ls(dm, curr_inode);
+
+    // mkdir(dm, curr_inode, "hello1");
+    // ls(dm, curr_inode);
+
+    //touch(dm, curr_inode, "hello");
+    //ls(dm, curr_inode);
+
+    //print_whole_dm(dm);
+
+    // rmdir(dm, curr_inode, "new");
+    // ls(dm, curr_inode);
+    cat(dm, 1, "hello.txt");
+
+    printf("\n\n\n");
+
+    echo(dm, 1, "hello.txt", 0);
+    printf("\n data :");
+    cat(dm, 1, "hello.txt");
     return 0;
 }
