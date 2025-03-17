5b33571db336608240b1297ba6da91e784dec50e
Timur <stukantimur811@gmail.com>
2025-03-17 22:20:51 +0200

    initialized echo func and small fixes to previouslly created func

--- fs.h
+++ fs.h
@@ -78,7 +78,7 @@ void print_whole_data_from_file_by_file_name(disk_mem *dm, uint32_t inode_number
 uint16_t create_new_file(disk_mem *dm);
 uint16_t create_file(disk_mem* dm, uint32_t inode_number_of_dir, char *file_name);
 int8_t add_data_to_file(disk_mem *dm, uint16_t file_inode_number, char *file_data, uint32_t data_size);
-int8_t add_data_to_file_by_dir(disk_mem *dm, uint16_t directory_inode_number, char *file_name, char *file_data, uint32_t data_size);
+int8_t add_data_to_file_by_dir(disk_mem *dm, uint16_t directory_inode_number, char *file_name, char *file_data, uint32_t data_size, uint8_t flag);
 
 int8_t check_if_name_is_present_in_dir(disk_mem *dm, uint16_t dir_inode, char *new_name);
 int32_t add_smth_to_dir(disk_mem *dm, uint16_t new_inode, uint16_t dir_inode, char *new_name);
@@ -99,4 +99,5 @@ int32_t touch(disk_mem *dm, uint32_t curr_dir_inode, char *file_name);
 int32_t rm(disk_mem *dm, uint32_t curr_dir_inode, char *file_name);
 int32_t rmdir(disk_mem *dm, uint32_t curr_dir_inode, char *dir_name);
 int32_t cat(disk_mem *dm, uint32_t curr_dir_inode, char *file_name);
+int32_t echo(disk_mem *dm, uint32_t curr_dir_inode, char *file_name, int flag);
 #endif 
